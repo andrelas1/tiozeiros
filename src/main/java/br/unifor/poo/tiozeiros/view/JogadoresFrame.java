5e5a62a0f2fa26efffd42f4c9108692cb30b6bc2
Andre Santos <andre.santos0906@gmail.com>
2016-11-15 15:37:25 -0300

    Tela de Cadastro de Jogador OK

--- src/main/java/br/unifor/poo/tiozeiros/view/JogadoresFrame.java
+++ src/main/java/br/unifor/poo/tiozeiros/view/JogadoresFrame.java
@@ -2,6 +2,7 @@ package br.unifor.poo.tiozeiros.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by andre on 12/11/16.
@@ -11,6 +12,8 @@ public class JogadoresFrame extends AbstractFrame {
     private JPanel contentPane;
     private Font titleFont = new Font("Serif", Font.BOLD, 14);
     private Font paragraphFont = new Font("Serif", Font.PLAIN, 12);
+    private int x = (MAX_WIDTH - 380) / 2;
+    private int y = (MAX_HEIGHT - 150) / 2;
 
     public void createFrame() {
 
@@ -21,11 +24,15 @@ public class JogadoresFrame extends AbstractFrame {
         JogadoresFrame frame = new JogadoresFrame();
         contentPane = gerarContentPane();
 
+        JLabel lblTitulo = new JLabel("Cadastro de Novo Jogador");
+        lblTitulo.setFont(new Font("Serif", Font.BOLD, 22));
+        lblTitulo.setBounds((MAX_WIDTH-310)/2, 20, 330, 60);
+        contentPane.add(lblTitulo);
+
         /**************************************
          DADOS PESSOAIS
          ***************************************/
-        int x = (MAX_WIDTH - 380) / 2;
-        int y = (MAX_HEIGHT - 150) / 2;
+
 
         JLabel lblNome = new JLabel("Nome");
         lblNome.setFont(titleFont);
@@ -49,43 +56,104 @@ public class JogadoresFrame extends AbstractFrame {
 
         JLabel lblHabilidades = new JLabel("Habilidades");
         lblHabilidades.setFont(titleFont);
-        lblHabilidades.setBounds(x, (int) jtTelefone.getBounds().getY()+60, 100, 28);
+        lblHabilidades.setBounds(x, (int) jtTelefone.getBounds().getY() + 60, 100, 28);
         contentPane.add(lblHabilidades);
 
         JLabel lblSeparator = new JLabel("-------------------------------------------------");
-        lblSeparator.setBounds(x, (int) jtTelefone.getBounds().getY()+80, MAX_WIDTH, 20);
+        lblSeparator.setBounds(x, (int) jtTelefone.getBounds().getY() + 80, MAX_WIDTH, 20);
         lblSeparator.setFont(new Font("Serif", Font.BOLD, 20));
         contentPane.add(lblSeparator);
 
         JLabel lblFinalização = new JLabel("Finalização");
-        lblFinalização.setBounds((int) lblSeparator.getBounds().getX(), (int) lblSeparator.getBounds().getY()+30, 100, 28);
+        lblFinalização.setBounds((int) lblSeparator.getBounds().getX(), (int) lblSeparator.getBounds().getY() + 30, 100, 28);
         lblFinalização.setFont(paragraphFont);
         contentPane.add(lblFinalização);
 
         JLabel lblPasse = new JLabel("Passe");
-        lblPasse.setBounds((int) lblSeparator.getBounds().getX(), (int) lblSeparator.getBounds().getY()+60, 100, 28);
+        lblPasse.setBounds((int) lblSeparator.getBounds().getX(), (int) lblFinalização.getBounds().getY() + 50, 100, 28);
         lblPasse.setFont(paragraphFont);
         contentPane.add(lblPasse);
 
         JLabel lblVelocidade = new JLabel("Velocidade");
-        lblVelocidade.setBounds((int) lblSeparator.getBounds().getX(), (int) lblSeparator.getBounds().getY()+90, 100, 28);
+        lblVelocidade.setBounds((int) lblSeparator.getBounds().getX(), (int) lblPasse.getBounds().getY() + 50, 100, 28);
         lblVelocidade.setFont(paragraphFont);
         contentPane.add(lblVelocidade);
 
         JLabel lblMarcação = new JLabel("Finalização");
-        lblMarcação.setBounds((int) lblSeparator.getBounds().getX(), (int) lblSeparator.getBounds().getY()+120, 100, 28);
+        lblMarcação.setBounds((int) lblSeparator.getBounds().getX(), (int) lblVelocidade.getBounds().getY() + 50, 100, 28);
         lblMarcação.setFont(paragraphFont);
         contentPane.add(lblMarcação);
 
         /**************************************
          HABILIDADES - NÍVEL
          ***************************************/
+        NivelHabilidades habilidadesFinalização = new NivelHabilidades();
+        habilidadesFinalização.setBounds((int) lblFinalização.getBounds().getX() + 90, (int) lblFinalização.getBounds().getY(), 300, 40);
+        contentPane.add(habilidadesFinalização);
 
+        NivelHabilidades habilidadesPasse = new NivelHabilidades();
+        habilidadesPasse.setBounds((int) lblPasse.getBounds().getX() + 90, (int) lblPasse.getBounds().getY(), 300, 40);
+        contentPane.add(habilidadesPasse);
 
+        NivelHabilidades habilidadesVelocidade = new NivelHabilidades();
+        habilidadesVelocidade.setBounds((int) lblVelocidade.getBounds().getX() + 90, (int) lblVelocidade.getBounds().getY(), 300, 40);
+        contentPane.add(habilidadesVelocidade);
+
+        NivelHabilidades habilidadesMarcação = new NivelHabilidades();
+        habilidadesMarcação.setBounds((int) lblMarcação.getBounds().getX() + 90, (int) lblMarcação.getBounds().getY(), 300, 40);
+        contentPane.add(habilidadesMarcação);
 
         frame.add(contentPane);
         mostrarTela(frame);
 
     }
 
+}
+
+class NivelHabilidades extends JPanel {
+    protected ArrayList<JCheckBox> checkBoxes = new ArrayList<>();
+    protected ArrayList<JLabel> labels = new ArrayList<>();
+    protected Integer niveis;
+
+    public NivelHabilidades() {
+        setLayout(new GridLayout(2, 1));
+        add(criarCheckBoxes(10));
+        add(criarLabels());
+        setBackground(new Color(7, 189, 62));
+    }
+
+    public JPanel criarCheckBoxes(int n) {
+        JPanel checkBoxPanel = new JPanel();
+        checkBoxPanel.setLayout(new GridLayout(1, 10));
+        niveis = n;
+        checkBoxPanel.setSize(300, 20);
+        for (int c = 0; c < n; c++) {
+            JCheckBox box = new JCheckBox();
+            box.setSize(16, 16);
+            box.setBackground(new Color(7, 189, 62));
+            checkBoxes.add(box);
+            checkBoxPanel.add(checkBoxes.get(c));
+        }
+        return checkBoxPanel;
+    }
+
+    public JPanel criarLabels() {
+        JPanel labelPanel = new JPanel();
+        labelPanel.setLayout(new GridLayout(1, 10));
+        labelPanel.setSize(300, 10);
+        labelPanel.setBackground(new Color(7,189,62));
+        for (int x = 1; x <= niveis; x++) {
+            JLabel label;
+            if (x >= 10) {
+                label = new JLabel(" " + Integer.toString(x));
+            } else {
+                label = new JLabel("  " + Integer.toString(x));
+            }
+            label.setSize(10, 10);
+            label.setBackground(new Color(7, 189, 62));
+            labels.add(label);
+            labelPanel.add(label);
+        }
+        return labelPanel;
+    }
 }
